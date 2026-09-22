3493fad1613d76f78374435e574055c75828421f
dghx23 <hough041@gmail.com>
2026-09-22 06:37:34 +0200

    Expose Victorian address autocomplete API

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const socialSecurity = require("./api/social-security.js");
 const propertyExtract = require("./api/property-extract.js");
+const addressAutocomplete = require("./api/address-autocomplete.js");
 
 const app = express();
 const port = Number(process.env.PORT || 10000);
@@ -16,6 +17,7 @@ app.get("/_health", (_req, res) => {
 app.use(express.json({ limit:"32kb" }));
 app.get("/api/social-security", socialSecurity);
 app.post("/api/property-extract", propertyExtract);
+app.get("/api/address-autocomplete", addressAutocomplete);
 
 app.use(express.static(root, {
   extensions: ["html"],
