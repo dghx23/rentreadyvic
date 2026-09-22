54af5552ed5988efa989dcdbf0fa6a3109c0576c
dghx23 <hough041@gmail.com>
2026-09-22 21:36:32 +0200

    Expose Core AU rental market API

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const socialSecurity = require("./api/social-security.js");
 const propertyExtract = require("./api/property-extract.js");
 const addressSearch = require("./api/address-search.js");
 const addressAutocomplete = require("./api/address-autocomplete.js");
+const rentalMarket = require("./api/rental-market.js");
 
 const app = express();
 const port = Number(process.env.PORT || 10000);
@@ -17,6 +18,7 @@ app.get("/_health", (_req, res) => {
 
 app.use(express.json({ limit:"32kb" }));
 app.get("/api/social-security", socialSecurity);
+app.get("/api/rental-market", rentalMarket);
 app.post("/api/property-extract", propertyExtract);
 app.get("/api/address-search", addressSearch);
 app.get("/api/address-autocomplete", addressAutocomplete);
